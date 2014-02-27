6b2eb54555c1c87fde7b8e69ecde30d7e4816936
Thomas Mailund <mailund@birc.au.dk>
2014-02-27 21:00:52 +0100

    Code cleanup

--- IMCoalHMM/CTMC.py
+++ IMCoalHMM/CTMC.py
@@ -1,42 +1,55 @@
+'''Code for contructing CTMCs and computing transition probabilities
+in them.'''
+
 from numpy import zeros
 from scipy import matrix
 from scipy.linalg import expm
 
 class CTMC(object):
-	'''Class representing the CTMC for the back-in-time coalescent.'''
-	
-	def __init__(self, state_space, rates_table):
-		'''Create the CTMC based on a state space and a mapping
-		from transition labels to rates.'''
-		
-		# Remember these, just to decouple state space from CTMC
-		# in other parts of the code...
-		self.B_states = state_space.B_states
-		self.L_states = state_space.L_states
-		self.R_states = state_space.R_states
-		self.E_states = state_space.E_states
-		
-		self.Q = matrix(zeros((len(state_space.states),len(state_space.states))))
-		
-		for src,trans,dst in state_space.transitions:
-			self.Q[src,dst] = rates_table[trans]
-		
-		for i in xrange(len(state_space.states)):
-			self.Q[i,i] = - self.Q[i,:].sum()
-	
-	def probability_matrix(self, delta_t):
-		'''Computes the transition probability matrix for a time period of delta_t.'''
-		return expm(self.Q * delta_t)
+    '''Class representing the CTMC for the back-in-time coalescent.'''
+
+    def __init__(self, state_space, rates_table):
+        '''Create the CTMC based on a state space and a mapping
+        from transition labels to rates.'''
+
+        # Remember these, just to decouple state space from CTMC
+        # in other parts of the code...
+        self.i11_index = state_space.i11_index
+        self.i12_index = state_space.i12_index
+        self.i22_index = state_space.i22_index
+
+        self.B_states = state_space.B_states
+        self.L_states = state_space.L_states
+        self.R_states = state_space.R_states
+        self.E_states = state_space.E_states
+
+        self.Q = matrix(zeros((len(state_space.states),
+                               len(state_space.states))))
+
+        for src, trans, dst in state_space.transitions:
+            self.Q[src, dst] = rates_table[trans]
+
+        for i in xrange(len(state_space.states)):
+            self.Q[i, i] = - self.Q[i, :].sum()
+
+    def probability_matrix(self, delta_t):
+        '''Computes the transition probability matrix for a
+        time period of delta_t.'''
+        return expm(self.Q * delta_t)
+
+
+def main():
+    '''Test'''
+    from IM2 import IM2, make_rates_table
+
+    state_space = IM2()
+    rates_table = make_rates_table(1, 1, 4e-4, 0.2, 0.2)
+
+    ctmc = CTMC(state_space, rates_table)
+    P = ctmc.probability_matrix(1.0)
+
+    print P[0,]
 
 
 if __name__ == '__main__':
-	from IM2 import IM2, make_rates_table
-	
-	state_space = IM2()
-	rates_table = make_rates_table(1,1,4e-4,0.2,0.2)
-	
-	ctmc = CTMC(state_space, rates_table)
-	P = ctmc.probability_matrix(1.0)
-	
-	print P[0,:]
-	
+    main()
--- IMCoalHMM/statespace_generator.py
+++ IMCoalHMM/statespace_generator.py
@@ -1,4 +1,7 @@
-iset = frozenset
+'''Code for constructing the state space of a coalescent system.
+'''
+
+import exceptions
 
 class CoalSystem(object):
     '''Abstract class for the two nucleotide coalescence system.
@@ -25,7 +28,7 @@ class CoalSystem(object):
     interpreted as if the transition can fire and
     non-deterministically produce different post sets; the lists
     should contain all those posts sets.
-    
+
     The one exeception is if the transition returns None.  This is
     interpreted as a guard violation and the transition update is
     aborted.
@@ -50,32 +53,31 @@ class CoalSystem(object):
         "C" for a coalescence event or "R" for a recombination event.
         '''
 
-        L = list(state)
+        tokens = list(state)
 
-        for ttype,tfunc in self.transitions[0]:
-            for token in L:
-                pre = iset([token])
+        for ttype, tfunc in self.transitions[0]:
+            for token in tokens:
+                pre = frozenset([token])
                 tproduct = tfunc(token)
-                for pop_a,pop_b,p in tproduct:
-                    post = p
+                for pop_a, pop_b, post in tfunc(token):
                     new_state = state.difference(pre).union(post)
                     yield ttype, pop_a, pop_b, new_state
 
-        for ttype,tfunc in self.transitions[1]:
-            for i in xrange(len(L)):
-                t1 = L[i]
+        for ttype, tfunc in self.transitions[1]:
+            for i in xrange(len(tokens)):
                 for j in xrange(i):
-                    t2 = L[j]
-                    
-                    pre = iset([t1,t2])
 
-                    pop_a, pop_b, tproduct = tfunc(t1,t2)
+                    pre = frozenset([tokens[i], tokens[j]])
+                    pop_a, pop_b, tproduct = tfunc(tokens[i], tokens[j])
                     if tproduct == None:
                         continue
                     post = tproduct
                     new_state = state.difference(pre).union(post)
                     yield ttype, pop_a, pop_b, new_state
 
+    def initial_state(self):
+        '''Virtual function for specifying the initial state.'''
+        raise exceptions.NotImplementedError()
 
     def compute_state_space(self):
         '''Computes the CTMC system.'''
@@ -84,34 +86,37 @@ class CoalSystem(object):
         seen = set(initial_states)
         unprocessed = initial_states
         self.state_numbers = {}
-        for i,s in enumerate(initial_states):
-            self.state_numbers[s] = i
+        for i, state in enumerate(initial_states):
+            self.state_numbers[state] = i
         edges = []
 
         while unprocessed:
-            s = unprocessed.pop()
-            n = self.state_numbers[s]
-            for t,pop_a,pop_b,ss in self.successors(s):
-                assert s != ss, "We don't like self-loops!"
+            state = unprocessed.pop()
+            state_no = self.state_numbers[state]
+            for trans, pop1, pop2, dest in self.successors(state):
+                assert state != dest, "We don't like self-loops!"
 
-                if ss not in self.state_numbers:
-                    self.state_numbers[ss] = len(self.state_numbers)
+                if dest not in self.state_numbers:
+                    self.state_numbers[dest] = len(self.state_numbers)
 
-                if ss not in seen:
-                    unprocessed.append(ss)
-                    seen.add(ss)
+                if dest not in seen:
+                    unprocessed.append(dest)
+                    seen.add(dest)
 
-                m = self.state_numbers[ss]
-                edges.append((n,(t,pop_a,pop_b),m))
+                edges.append((state_no,
+                             (trans, pop1, pop2),
+                             self.state_numbers[dest]))
 
         remapping = {}
         mapped_state_numbers = {}
-        for v in set(self.state_numbers.values()):
-            remapping[v] = len(remapping)
-        for k, v in self.state_numbers.iteritems():
-            mapped_state_numbers[k] = remapping[v]
+        for state_no in set(self.state_numbers.values()):
+            remapping[state_no] = len(remapping)
+        for state, state_no in self.state_numbers.iteritems():
+            mapped_state_numbers[state] = remapping[state_no]
         self.state_numbers = mapped_state_numbers
-        edges = [(remapping[a],(t,pa,pb),remapping[b]) for a,(t,pa,pb),b in edges]
+        edges = [(remapping[src], (trans, pop1, pop2), remapping[dest]) \
+                    for src, (trans, pop1, pop2), dest in edges]
+
         return self.state_numbers, edges
 
 
@@ -121,14 +126,16 @@ class BasicCoalSystem(CoalSystem):
 
     def recombination(self, token):
         '''Compute the tokens we get from a recombination on "token".
-        
+
         Returns None if the recombination would just return
         "token" again, so we avoid returning "empty" tokens.
         '''
         _, nucs = token
         left, right = nucs
-        if not (left and right): return []
-        return [(0, 0, iset([(0,(left,iset())), (0,(iset(),right))]))]
+        if not (left and right):
+            return []
+        return [(0, 0, frozenset([(0, (left, frozenset())),
+                             (0, (frozenset(), right))]))]
 
     def coalesce(self, token1, token2):
         '''Construct a new token by coalescening "token1" and "token2".'''
@@ -137,7 +144,7 @@ class BasicCoalSystem(CoalSystem):
         left1, right1 = nuc1
         left2, right2 = nuc2
         left, right = left1.union(left2), right1.union(right2)
-        return 0, 0, iset([(0, (left, right))])
+        return 0, 0, frozenset([(0, (left, right))])
 
     def initial_state(self):
         '''Build the initial state for this system.
@@ -150,8 +157,10 @@ class BasicCoalSystem(CoalSystem):
 
     def __init__(self, species):
         CoalSystem.__init__(self)
-        self.transitions = [[('R',self.recombination)], [('C',self.coalesce)]]
-        self.init = [iset([(0,(iset([s]),iset([s]))) for s in species])]
+        self.transitions = [[('R', self.recombination)],
+                            [('C', self.coalesce)]]
+        self.init = [frozenset([(0, (frozenset([s]), frozenset([s]))) \
+                            for s in species])]
 
 
 
@@ -160,31 +169,35 @@ class IM(CoalSystem):
 
     def recombination(self, token):
         '''Compute the tokens we get from a recombination on "token".
-        
+
         Returns None if the recombination would just return
         "token" again, so we avoid returning "empty" tokens.
         '''
         pop, nucs = token
         left, right = nucs
-        if not (left and right): return []
-        return [(pop,pop, iset([(pop,(left,iset())), (pop,(iset(),right))]))]
+        if not (left and right):
+            return []
+        return [(pop, pop, frozenset([(pop, (left, frozenset())),
+                                 (pop, (frozenset(), right))]))]
 
     def coalesce(self, token1, token2):
         '''Construct a new token by coalescening "token1" and "token2".'''
         pop1, nuc1 = token1
         pop2, nuc2 = token2
-        if pop1 != pop2: return -1, -1, None # abort transition...
-        
-        
+        if pop1 != pop2:
+            return -1, -1, None # abort transition...
+
+
         left1, right1 = nuc1
         left2, right2 = nuc2
         left, right = left1.union(left2), right1.union(right2)
-        return pop1, pop2, iset([(pop1, (left, right))])
+        return pop1, pop2, frozenset([(pop1, (left, right))])
 
     def migrate(self, token):
         '''Move nucleotides from one population to another'''
         pop, nuc = token
-        res = [(pop,pop2,iset([(pop2,nuc)])) for pop2 in self.legal_migrations[pop]]
+        res = [(pop, pop2, frozenset([(pop2, nuc)])) \
+                for pop2 in self.legal_migrations[pop]]
         return res
 
     def initial_state(self):
@@ -198,31 +211,40 @@ class IM(CoalSystem):
 
     def __init__(self, species):
         CoalSystem.__init__(self)
-        
+
         self.legal_migrations = dict()
-        for s in species:
-        	self.legal_migrations[s] = iset([ss for ss in species if s != ss])
-        
-        self.transitions = [[('R',self.recombination),('M',self.migrate)], 
-        					[('C',self.coalesce)]]
-        self.init = [iset([(s,(iset([s]),iset([s]))) for s in species])]
+        for sample in species:
+            self.legal_migrations[sample] = \
+                frozenset([other for other in species if sample != other])
+
+        self.transitions = [[('R', self.recombination),
+                             ('M', self.migrate)],
+        					[('C', self.coalesce)]]
+        self.init = [frozenset([(sample,
+                                (frozenset([sample]),
+                                 frozenset([sample])))
+                        for sample in species])]
 
 
 def pretty_lineage(lin):
     '''Pretty representation of a lineage.'''
     return ''.join(str(e) for e in lin)
 
-def pretty_state(s):
+def pretty_state(state):
     '''Pretty textual representation of a state ...'''
-    return [(pop,pretty_lineage(lin)) for (pop,lin) in s]
+    return [(pop, pretty_lineage(lin)) for (pop, lin) in state]
 
-def pretty_coal_class(s):
+def pretty_coal_class(state):
     '''Pretty textual representation of a state, showing not the
     populations but only which lineages have coalesced.'''
-    return '/'.join(pretty_lineage(lin) for _,lin in s)
+    return '/'.join(pretty_lineage(lin) for _, lin in state)
 
-if __name__ == "__main__":
-    system = IM(range(2))
-    states, transitions =  system.compute_state_space()
+
+def main():
+    '''Test.'''
+    state_space = IM(range(2))
+    states, transitions = state_space.compute_state_space()
     print len(states), 'and', len(transitions), 'transitions'
 
+if __name__ == "__main__":
+    main()
