0e5397bb6758dddffee7ef61b08fbd0bc1fe5dcb
Thomas Mailund <mailund@birc.au.dk>
2014-03-03 17:29:29 +0100

    Transition calculates.
    Proper break points are not done yet, and I'm far from having the optimiser done.

--- src/IMCoalHMM/variable_coalescence_rate_isolation_model.py
+++ src/IMCoalHMM/variable_coalescence_rate_isolation_model.py
@@ -1,92 +1,52 @@
 '''Code for constructing and optimizing the HMM for an isolation model.
 '''
 
-from numpy import zeros, matrix
+from numpy import zeros, identity, matrix
 from numpy.testing import assert_almost_equal
 
-from IMCoalHMM.statespace_generator import Single, Isolation
+from IMCoalHMM.isolation_model import Isolation2, make_rates_table_isolation
+from IMCoalHMM.isolation_model import Single2, make_rates_table_single
+
 from IMCoalHMM.CTMC import CTMC
 from IMCoalHMM.transitions import CTMCSystem
 from IMCoalHMM.transitions import compute_transition_probabilities
 from IMCoalHMM.break_points import exp_break_points
 from IMCoalHMM.emissions import emission_matrix
 
-## State space code ############################################
-class Isolation2(Isolation):
-    '''Class for IM system with exactly two samples.'''
-
-    def __init__(self):
-        '''Constructs the state space and collect B, L, R and E states (see the
-        CoalHMM papers for what these are).'''
-
-        super(Isolation2, self).__init__([1, 2])
-        self.compute_state_space()
-
-        i12_state = frozenset([(sample,
-                                (frozenset([sample]), frozenset([sample])))
-                                for sample in [1, 2]])
-        self.i12_index = self.states[i12_state]
-
-def make_rates_table_isolation(C1, C2, R):
-    '''Builds the rates table from the CTMC for the two-samples system.'''
-    table = dict()
-    table[('C', 1, 1)] = C1
-    table[('C', 2, 2)] = C2
-    table[('R', 1, 1)] = R
-    table[('R', 2, 2)] = R
-    return table
-
-
-class Single2(Single):
-    '''Class for a merged ancestral population'''
-
-    def __init__(self):
-        '''Constructs the state space and collect B, L, R and E states (see the
-        CoalHMM papers for what these are).'''
-
-        super(Single2, self).__init__()
-        self.compute_state_space()
-
-def make_rates_table_single(C, R):
-    '''Builds the rates table from the CTMC for the two-samples system.'''
-    table = dict()
-    table[('C', 0, 0)] = C
-    table[('R', 0, 0)] = R
-    return table
 
 ## Code for computing HMM transition probabilities ####################
-def _compute_through(single, break_points):
+def _compute_through(ctmcs, break_points):
     '''Computes the matrices for moving through an interval'''
     no_states = len(break_points)
 
     # Construct the transition matrices for going through each interval
-    through = [single.probability_matrix(break_points[i+1] - break_points[i])
+    through = [ctmcs[i].probability_matrix(break_points[i+1] - break_points[i])
                for i in xrange(no_states - 1)]
 
     # As a hack we set up a pseudo through matrix for the last interval that
     # just puts all probability on ending in one of the end states. This
     # simplifies the HMM transition probability code as it avoids a special case
     # for the last interval.
-    pseudo_through = matrix(zeros((len(single.state_space.states),
-                                   len(single.state_space.states))))
-    pseudo_through[:, single.state_space.end_states[0]] = 1.0
+    pseudo_through = matrix(zeros((len(ctmcs[-1].state_space.states),
+                                   len(ctmcs[-1].state_space.states))))
+    pseudo_through[:, ctmcs[-1].state_space.end_states[0]] = 1.0
     through.append(pseudo_through)
 
     return through
 
-def _compute_upto(isolation, single, break_points, through):
+def _compute_upto(isolation, ctmcs, break_points, through):
     '''Computes the probability matrices for moving from time zero up to,
     but not through, interval i.'''
 
     no_states = len(break_points)
 
     # Projection matrix needed to go from the isolation to the single
-    # state spaces
+    # state spaces.
     projection = matrix(zeros((len(isolation.state_space.states),
-                               len(single.state_space.states))))
+                               len(ctmcs[0].state_space.states))))
     for state, isolation_index in isolation.state_space.states.items():
         ancestral_state = frozenset([(0, nucs) for (_, nucs) in state])
-        ancestral_index = single.state_space.states[ancestral_state]
+        ancestral_index = ctmcs[0].state_space.states[ancestral_state]
         projection[isolation_index, ancestral_index] = 1.0
 
     # We handle the first state as a special case because of the isolation
@@ -98,36 +58,38 @@ def _compute_upto(isolation, single, break_points, through):
 
     return upto
 
-def _compute_between(single, break_points):
+def _compute_between(ctmcs, through):
     '''Computes the matrices for moving from the end of interval i
     to the beginning of interval j.'''
 
-    no_states = len(break_points)
+    no_states = len(ctmcs)
+    between = dict()
     # Transitions going from the endpoint of interval i to the entry point
     # of interval j
-    return dict(
-        ((i, j), single.probability_matrix(break_points[j] - break_points[i+1]))
-        for i in xrange(no_states - 1)
-        for j in xrange(i+1, no_states)
-    )
+    for i in xrange(no_states-1):
+        between[(i,i+1)] = matrix(identity(len(ctmcs[0].state_space.states)))
+        for j in xrange(i+2, no_states):
+            between[(i,j)] = between[(i,j-1)] * through[j-1]
+    return between
 
-
-class IsolationCTMCSystem(CTMCSystem):
+class VariableCoalRateCTMCSystem(CTMCSystem):
     '''Wrapper around CTMC transition matrices for the isolation model.'''
 
-    def __init__(self, isolation_ctmc, single_ctmc, break_points):
+    def __init__(self, isolation_ctmc, single_ctmcs, break_points):
         '''Construct all the matrices and cache them for the
         method calls.'''
 
-        self.no_states_ = len(break_points)
+        self.no_states_ = len(single_ctmcs)
         self.initial_ = isolation_ctmc.state_space.i12_index
-        self.begin_states_ = single_ctmc.state_space.begin_states
-        self.left_states_ = single_ctmc.state_space.left_states
-        self.end_states_ = single_ctmc.state_space.end_states
+        # Even though we have different CTMCs they have the same state space
+        self.begin_states_ = single_ctmcs[0].state_space.begin_states
+        self.left_states_ = single_ctmcs[0].state_space.left_states
+        self.end_states_ = single_ctmcs[0].state_space.end_states
 
-        self.through_ = _compute_through(single_ctmc, break_points)
-        self.upto_ = _compute_upto(isolation_ctmc, single_ctmc, break_points, self.through_)
-        self.between_ = _compute_between(single_ctmc, break_points)
+        self.through_ = _compute_through(single_ctmcs, break_points)
+        self.upto_ = _compute_upto(isolation_ctmc, single_ctmcs,
+                                   break_points, self.through_)
+        self.between_ = _compute_between(single_ctmcs, self.through_)
 
     @property
     def no_states(self):
@@ -167,37 +129,62 @@ class IsolationCTMCSystem(CTMCSystem):
 
 
 ## Class that can construct HMMs ######################################
-class IsolationModel(object):
-    '''Class wrapping the code that generates an isolation model HMM.'''
+from IMCoalHMM.transitions import compute_transition_probabilities
+from IMCoalHMM.break_points import exp_break_points
+from IMCoalHMM.emissions import emission_matrix
+
+class VariableCoalescenceRateIsolationModel(object):
+    '''Class wrapping the code that generates an isolation model HMM
+    with variable coalescence rates in the different intervals.'''
     
     def __init__(self):
         '''Construct the model.
         
-        This builds the state spaces for the CTMCs but not the matrices for the
+        This builds the state spaces for the CTMCs but the matrices for the
         HMM since those will depend on the rate parameters.'''
-        super(IsolationModel, self).__init__()
+        super(VariableCoalescenceRateIsolationModel, self).__init__()
         self.isolation_state_space = Isolation2()
         self.single_state_space = Single2()
         
 
-    def build_HMM(self, no_states, split_time, coal_rate, recomb_rate):
+    def build_HMM(self, split_time, coal_rates, recomb_rate):
         '''Construct CTMCs and compute HMM matrices given the split time
-        and the rates.'''
+        and the rates.
+        
+        The split_time parameter is for setting a period where it is impossible
+        for the two samples to coalesce (an isolation model). If it is
+        set to 0.0 the system will work as Li & Durbin (2011)'s PSMC.
+        
+        The coal_rates list should contain a coalescence rate for each interval
+        in the model (except for the time up to split_time). It determins
+        both the number of states and the transition probabilities.
+        In optimisation it should be constrained somewhat since a free 
+        rate for each interval will not be possible to estimate, but
+        this is left to functionality outside the model.
+        '''
 
         # We assume here that the coalescence rate is the same in the two
-        # separate populations as it is in the ancestral. This is not necessarily
-        # true but it worked okay in simulations in Mailund et al. (2011).
-        isolation_rates = make_rates_table_isolation(coal_rate, coal_rate, recomb_rate)
-        isolation_ctmc = CTMC(self.isolation_state_space, isolation_rates)
-        single_rates = make_rates_table_single(coal_rate, recomb_rate)
-        single_ctmc = CTMC(self.single_state_space, single_rates)
+        # separate populations as in the ancestral just before teh split.
+        # This is not necessarily true but it worked okay in simulations
+        # in Mailund et al. (2011).
         
-        break_points = exp_break_points(no_states, coal_rate, split_time)
-        ctmc_system = IsolationCTMCSystem(isolation_ctmc, single_ctmc, break_points)
+        isolation_rates = make_rates_table_isolation(coal_rates[0], coal_rates[0],
+                                                     recomb_rate)
+        isolation_ctmc = CTMC(self.isolation_state_space, isolation_rates)
+        
+        single_ctmcs = []
+        for coal_rate in coal_rates:
+            single_rates = make_rates_table_single(coal_rate, recomb_rate)
+            single_ctmc = CTMC(self.single_state_space, single_rates)
+            single_ctmcs.append(single_ctmc)
+        
+        # FIXME: need different code to set this one!
+        break_points = exp_break_points(len(coal_rates), coal_rates[0], split_time)
 
+        ctmc_system = VariableCoalRateCTMCSystem(isolation_ctmc, 
+                                                 single_ctmcs, break_points)
         pi, T = compute_transition_probabilities(ctmc_system)
         E = emission_matrix(break_points, coal_rate)
-
         return pi, T, E
 
 
@@ -225,11 +212,14 @@ class MinimizeWrapper(object):
 def main():
     '''Test'''
 
-    model = IsolationModel()
-    pi, T, E = model.build_HMM(4, 1.0, 0.5, 4e-4)
+    model = VariableCoalescenceRateIsolationModel()
+    split_time = 1.1
+    coal_rates = [1.0, 1.0, 1.0, 1.0]
+    recomb_rate = 4e-4
+    pi, T, E = model.build_HMM(split_time, coal_rates, recomb_rate)
 
     no_states = pi.getHeight()
-    assert no_states == 4
+    assert no_states == len(coal_rates)
 
     pi_sum = 0.0
     for row in xrange(no_states):
