2709f594a624cd46158154db99f8b56fc5eb7921
Thomas Mailund <mailund@birc.au.dk>
2014-03-05 22:07:11 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 from setuptools import setup, find_packages
 setup(
     name = "IMCoalHMM",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(where='src'),
     package_dir = {'': 'src'},
     
