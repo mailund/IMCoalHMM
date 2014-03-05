1e4fc53b72a3380e3faf63004b34e4a07b121555
Thomas Mailund <mailund@birc.au.dk>
2014-03-05 21:22:31 +0100

    A bunch of code cleanups to satisfy PyCharm

--- src/IMCoalHMM/isolation_with_migration_model.py
+++ src/IMCoalHMM/isolation_with_migration_model.py
@@ -1,9 +1,8 @@
-'''Code for constructing and optimizing the HMM for an isolation model.
-'''
+"""Code for constructing and optimizing the HMM for an isolation model.
+"""
 
 from numpy import zeros, matrix, identity
 from numpy.testing import assert_almost_equal
-
 
 from IMCoalHMM.statespace_generator import Migration
 from IMCoalHMM.CTMC import CTMC
@@ -18,53 +17,51 @@ from IMCoalHMM.isolation_model import Single2, make_rates_table_single
 
 ## State space code ############################################
 class Migration2(Migration):
-    '''Class for IM system with exactly two samples.'''
+    """Class for IM system with exactly two samples."""
 
     def __init__(self):
-        '''Constructs the state space and collect B, L, R and E states (see the
+        """Constructs the state space and collect B, L, R and E states (see the
         CoalHMM papers for what these are).
 
         Also collects the indices in the state space for the three
         (realistic) initial states, with both chromosomes in population 1
-        or in 2 or one from each.'''
+        or in 2 or one from each."""
 
         super(Migration2, self).__init__([1, 2])
-        
 
         self.compute_state_space()
 
-        i11_state = frozenset([(1,
-                                (frozenset([sample]), frozenset([sample])))
-                                for sample in [1, 2]])
-        i22_state = frozenset([(2,
-                                (frozenset([sample]), frozenset([sample])))
-                                for sample in [1, 2]])
-        i12_state = frozenset([(sample,
-                                (frozenset([sample]), frozenset([sample])))
-                                for sample in [1, 2]])
+        i11_state = frozenset([(1, (frozenset([sample]), frozenset([sample])))
+                               for sample in [1, 2]])
+        i22_state = frozenset([(2, (frozenset([sample]), frozenset([sample])))
+                               for sample in [1, 2]])
+        i12_state = frozenset([(sample, (frozenset([sample]), frozenset([sample])))
+                               for sample in [1, 2]])
 
         self.i11_index = self.states[i11_state]
         self.i12_index = self.states[i12_state]
         self.i22_index = self.states[i22_state]
 
 
-def make_rates_table_migration(C1, C2, R, M12, M21):
-    '''Builds the rates table from the CTMC for the two-samples system.'''
+def make_rates_table_migration(coal_rate_1, coal_rate_2, recomb_rate,
+                               migration_rate_12, migration_rate_21):
+    """Builds the rates table from the CTMC for the two-samples system.
+    """
     table = dict()
-    table[('C', 1, 1)] = C1
-    table[('C', 2, 2)] = C2
-    table[('R', 1, 1)] = R
-    table[('R', 2, 2)] = R
-    table[('M', 1, 2)] = M12
-    table[('M', 2, 1)] = M21
+    table[('C', 1, 1)] = coal_rate_1
+    table[('C', 2, 2)] = coal_rate_2
+    table[('R', 1, 1)] = recomb_rate
+    table[('R', 2, 2)] = recomb_rate
+    table[('M', 1, 2)] = migration_rate_12
+    table[('M', 2, 1)] = migration_rate_21
     return table
 
 
 ## Code for computing HMM transition probabilities ####################
-def _compute_through(migration, migration_break_points, 
+def _compute_through(migration, migration_break_points,
                      ancestral, ancestral_break_points):
-    '''Computes the matrices for moving through an interval'''
-    
+    """Computes the matrices for moving through an interval"""
+
     # Projection matrix needed to go from the migration to the single
     # state spaces
     projection = matrix(zeros((len(migration.state_space.states),
@@ -73,20 +70,19 @@ def _compute_through(migration, migration_break_points,
         ancestral_state = frozenset([(0, nucs) for (_, nucs) in state])
         ancestral_index = ancestral.state_space.states[ancestral_state]
         projection[isolation_index, ancestral_index] = 1.0
-    
+
     no_migration_states = len(migration_break_points)
     no_ancestral_states = len(ancestral_break_points)
 
     # Construct the transition matrices for going through each interval in
     # the migration phase
-    migration_through = [migration.probability_matrix(migration_break_points[i+1] - migration_break_points[i])
+    migration_through = [migration.probability_matrix(migration_break_points[i + 1] - migration_break_points[i])
                          for i in xrange(no_migration_states - 1)]
     last_migration = migration.probability_matrix(ancestral_break_points[0] - migration_break_points[-1]) \
-                   * projection
+                     * projection
     migration_through.append(last_migration)
-    
-    
-    ancestral_through = [ancestral.probability_matrix(ancestral_break_points[i+1] - ancestral_break_points[i])
+
+    ancestral_through = [ancestral.probability_matrix(ancestral_break_points[i + 1] - ancestral_break_points[i])
                          for i in xrange(no_ancestral_states - 1)]
 
     # As a hack we set up a pseudo through matrix for the last interval that
@@ -100,9 +96,10 @@ def _compute_through(migration, migration_break_points,
 
     return migration_through + ancestral_through
 
-def _compute_upto(isolation, migration, ancestral, break_points, through):
-    '''Computes the probability matrices for moving from time zero up to,
-    but not through, interval i.'''
+
+def _compute_upto(isolation, migration, break_points, through):
+    """Computes the probability matrices for moving from time zero up to,
+    but not through, interval i."""
 
     no_states = len(break_points)
 
@@ -119,169 +116,166 @@ def _compute_upto(isolation, migration, ancestral, break_points, through):
     upto = [None] * no_states
     upto[0] = isolation.probability_matrix(break_points[0]) * projection
     for i in xrange(1, no_states):
-        upto[i] = upto[i-1] * through[i-1]
+        upto[i] = upto[i - 1] * through[i - 1]
 
     return upto
 
+
 def _compute_between(through):
-    '''Computes the matrices for moving from the end of interval i
-    to the beginning of interval j.'''
+    """Computes the matrices for moving from the end of interval i
+    to the beginning of interval j."""
 
     no_states = len(through)
     between = dict()
     # Transitions going from the endpoint of interval i to the entry point
     # of interval j
-    for i in xrange(no_states-1):
-        between[(i,i+1)] = matrix(identity(through[i].shape[1]))
-        for j in xrange(i+2, no_states):
-            between[(i,j)] = between[(i,j-1)] * through[j-1]
+    for i in xrange(no_states - 1):
+        between[(i, i + 1)] = matrix(identity(through[i].shape[1]))
+        for j in xrange(i + 2, no_states):
+            between[(i, j)] = between[(i, j - 1)] * through[j - 1]
     return between
 
 
 class IsolationMigrationCTMCSystem(CTMCSystem):
-    '''Wrapper around CTMC transition matrices for the isolation model.'''
+    """Wrapper around CTMC transition matrices for the isolation model."""
 
     def __init__(self, isolation_ctmc, migration_ctmc, ancestral_ctmc,
                  migration_break_points, ancestral_break_points):
-        '''Construct all the matrices and cache them for the
-        method calls.'''
+        """Construct all the matrices and cache them for the
+        method calls."""
 
         self.no_migration_states = len(migration_break_points)
         self.no_ancestral_states = len(ancestral_break_points)
         self.no_states_ = self.no_migration_states + self.no_ancestral_states
 
         self.initial_ = isolation_ctmc.state_space.i12_index
-        
+
         self.begin_states_ = [migration_ctmc.state_space.begin_states,
                               ancestral_ctmc.state_space.begin_states]
-        self.left_states_  = [migration_ctmc.state_space.left_states,
-                              ancestral_ctmc.state_space.left_states]
-        self.end_states_   = [migration_ctmc.state_space.end_states,
-                              ancestral_ctmc.state_space.end_states]
+        self.left_states_ = [migration_ctmc.state_space.left_states,
+                             ancestral_ctmc.state_space.left_states]
+        self.end_states_ = [migration_ctmc.state_space.end_states,
+                            ancestral_ctmc.state_space.end_states]
 
         break_points = list(migration_break_points) + list(ancestral_break_points)
 
         self.through_ = _compute_through(migration_ctmc, migration_break_points,
                                          ancestral_ctmc, ancestral_break_points)
-        self.upto_ = _compute_upto(isolation_ctmc, migration_ctmc, ancestral_ctmc,
-                                   break_points, self.through_)
+        self.upto_ = _compute_upto(isolation_ctmc, migration_ctmc, break_points, self.through_)
         self.between_ = _compute_between(self.through_)
 
     def _is_ancestral(self, i):
-        '''Is index i in the ancestral populations?'''
+        """Is index i in the ancestral populations?"""
         return self.no_migration_states <= i
 
     @property
     def no_states(self):
-        "The number of states the HMM should have."
+        """The number of states the HMM should have."""
         return self.no_states_
 
     @property
     def initial(self):
-        'The initial state index in the bottom-most matrix'
+        """The initial state index in the bottom-most matrix"""
         return self.initial_
 
     def begin_states(self, i):
-        'Begin states for interval i.'
+        """Begin states for interval i."""
         return self.begin_states_[self._is_ancestral(i)]
 
     def left_states(self, i):
-        'Left states for interval i.'
+        """Left states for interval i."""
         return self.left_states_[self._is_ancestral(i)]
 
     def end_states(self, i):
-        'End states for interval i.'
+        """End states for interval i."""
         return self.end_states_[self._is_ancestral(i)]
 
     def through(self, i):
-        'Returns a probability matrix for going through interval i'
+        """Returns a probability matrix for going through interval i"""
         return self.through_[i]
 
     def upto(self, i):
-        '''Returns a probability matrix for going up to, but not
-        through, interval i'''
+        """Returns a probability matrix for going up to, but not
+        through, interval i"""
         return self.upto_[i]
 
     def between(self, i, j):
-        '''Returns a probability matrix for going from the
-        end of interval i up to (but not through) interval j'''
+        """Returns a probability matrix for going from the
+        end of interval i up to (but not through) interval j"""
         return self.between_[(i, j)]
 
 
 ## Class that can construct HMMs ######################################
 class IsolationMigrationModel(object):
-    '''Class wrapping the code that generates an isolation model HMM.'''
-    
+    """Class wrapping the code that generates an isolation model HMM."""
+
     def __init__(self):
-        '''Construct the model.
-        
+        """Construct the model.
+
         This builds the state spaces for the CTMCs but not the matrices for the
-        HMM since those will depend on the rate parameters.'''
+        HMM since those will depend on the rate parameters."""
         super(IsolationMigrationModel, self).__init__()
         self.isolation_state_space = Isolation2()
         self.migration_state_space = Migration2()
         self.single_state_space = Single2()
-        
 
-    def build_HMM(self, no_mig_states, no_ancestral_states,
-                  isolation_time, migration_time, 
+    def build_hidden_markov_model(self, no_mig_states, no_ancestral_states,
+                  isolation_time, migration_time,
                   coal_rate, recomb_rate, mig_rate):
-        '''Construct CTMCs and compute HMM matrices given the split times
-        and the rates.'''
+        """Construct CTMCs and compute HMM matrices given the split times
+        and the rates."""
 
         # We assume here that the coalescence rate is the same in the two
         # separate populations as it is in the ancestral. This is not necessarily
         # true but it worked okay in simulations in Mailund et al. (2012).
-        
+
         isolation_rates = make_rates_table_isolation(coal_rate, coal_rate, recomb_rate)
         isolation_ctmc = CTMC(self.isolation_state_space, isolation_rates)
-        
+
         migration_rates = make_rates_table_migration(coal_rate, coal_rate, recomb_rate,
                                                      mig_rate, mig_rate)
         migration_ctmc = CTMC(self.migration_state_space, migration_rates)
-        
+
         single_rates = make_rates_table_single(coal_rate, recomb_rate)
         single_ctmc = CTMC(self.single_state_space, single_rates)
-        
+
         tau1 = isolation_time
         tau2 = isolation_time + migration_time
         migration_break_points = uniform_break_points(no_mig_states, tau1, tau2)
         ancestral_break_points = exp_break_points(no_ancestral_states, coal_rate, tau2)
         break_points = list(migration_break_points) + list(ancestral_break_points)
-        
+
         ctmc_system = IsolationMigrationCTMCSystem(isolation_ctmc, migration_ctmc, single_ctmc,
                                                    migration_break_points, ancestral_break_points)
 
-        pi, T = compute_transition_probabilities(ctmc_system)
-        E = emission_matrix(break_points, coal_rate)
+        initial_probs, transition_probs = compute_transition_probabilities(ctmc_system)
+        emission_probs = emission_matrix(break_points, coal_rate)
 
-        return pi, T, E
+        return initial_probs, transition_probs, emission_probs
 
 
 ## Wrapper for maximum likelihood optimization ###############################
 class MinimizeWrapper(object):
-    '''Callable object wrapping the log likelihood computation for maximum
-    liklihood estimation.'''
-    
-    def __init__(self, logL, no_migration_states, no_ancestral_states):
-        '''Wrap the log likelihood computation with the non-variable parameter
-        which is the number of states.'''
-        self.logL = logL
+    """Callable object wrapping the log likelihood computation for maximum
+    liklihood estimation."""
+
+    def __init__(self, log_likelihood, no_migration_states, no_ancestral_states):
+        """Wrap the log likelihood computation with the non-variable parameter
+        which is the number of states."""
+        self.log_likelihood = log_likelihood
         self.no_migration_states = no_migration_states
         self.no_ancestral_states = no_ancestral_states
-        
+
     def __call__(self, parameters):
-        '''Compute the likelihood in a paramter point. It computes -logL since
-        the optimizer will minimize the function.'''
+        """Compute the likelihood in a paramter point. It computes -logL since
+        the optimizer will minimize the function."""
         if min(parameters) <= 0:
-            return 1e18 # fixme: return infinity
-        return -self.logL(self.no_migration_states, self.no_ancestral_states, *parameters)
-
-
+            return 1e18  # fixme: return infinity
+        return -self.log_likelihood(self.no_migration_states, self.no_ancestral_states, *parameters)
 
 
 def main():
-    '''Test'''
+    """Test"""
 
     no_mig_states = 4
     no_ancestral_states = 4
@@ -292,8 +286,8 @@ def main():
     mig_rate = 0.1
 
     model = IsolationMigrationModel()
-    pi, T, E = model.build_HMM(no_mig_states, no_ancestral_states,
-                               isolation_time, migration_time, 
+    pi, T, E = model.build_hidden_markov_model(no_mig_states, no_ancestral_states,
+                               isolation_time, migration_time,
                                coal_rate, recomb_rate, mig_rate)
 
     no_states = pi.getHeight()
@@ -307,11 +301,11 @@ def main():
     assert no_states == T.getWidth()
     assert no_states == T.getHeight()
 
-    T_sum = 0.0
+    transitions_sum = 0.0
     for row in xrange(no_states):
         for col in xrange(no_states):
-            T_sum += T[row, col]
-    assert_almost_equal(T_sum, no_states)
+            transitions_sum += T[row, col]
+    assert_almost_equal(transitions_sum, no_states)
 
     print 'Done'
 
--- src/IMCoalHMM/likelihood.py
+++ src/IMCoalHMM/likelihood.py
@@ -1,41 +1,52 @@
-'''Code for combining data with a model for computing and maximising
+"""Code for combining data with a model for computing and maximising
 likelihoods.
-'''
+"""
+
 
 class Likelihood(object):
-    '''Combining model and data.'''
+    """Combining model and data."""
 
     def __init__(self, model, forwarder):
-        '''Bind a model to sequence data in the form of a zipHMM Forwarder.'''
+        """Bind a model to sequence data in the form of a zipHMM Forwarder."""
         super(Likelihood, self).__init__()
         self.model = model
         self.forwarder = forwarder
-    
+
     def __call__(self, *parameters):
-        '''Compute the log-likelihood at a set of parameters.'''
-        pi, T, E = self.model.build_HMM(*parameters)
-        return self.forwarder.forward(pi, T, E)
+        """Compute the log-likelihood at a set of parameters."""
+        pi, trans_probs, emission_probs = self.model.build_hidden_markov_model(*parameters)
+        return self.forwarder.forward(pi, trans_probs, emission_probs)
+
 
 import scipy.optimize
+
+
 def maximum_likelihood_estimate(wrapper, initial_parameters,
-                                optimizer = scipy.optimize.fmin,
-                                log_file = None, 
+                                optimizer=scipy.optimize.fmin,
+                                log_file=None,
                                 log_param_transform=lambda x: x):
-    '''Maximum likelihood estimation.
-    
+    """Maximum likelihood estimation.
+
     This function requires a wrapper around the likelihood computation
-    that will be model specific, splitting the paramters to be optimized from those
+    that will be model specific, splitting the parameters to be optimized from those
     that should not, such as number of states, epochs etc.
-    
+
     If a log file files is provided, the optimisation is logged to that file through
     a call back. The "log_param_transform" function provides a handle to transform
     the parameters to a different space for logging.
-    
+
     It also requires an initial parameter point for the optimization, and a numerical
     algorithm for optimisation (a default from scipy is fmin).
-    
-    The function returns the maximum likelihood parameters.
-    '''
+
+    :param wrapper: The wrapper needed for computing the likelihood.
+    :param initial_parameters: The initial set of parameters. Model specific.
+    :param optimizer: The algorithm used for numerical optimization.
+    :param log_file: Progress will be logged to this file/stream.
+    :param log_param_transform: A function to map the optimization parameter space
+     into a model parameter space.
+
+    :returns: the maximum likelihood parameters.
+    """
     log_callback = None
     if log_file:
         def log_callback(parameters):
@@ -43,4 +54,3 @@ def maximum_likelihood_estimate(wrapper, initial_parameters,
             print >> log_file, '\t'.join(log_params)
 
     return optimizer(wrapper, initial_parameters, callback=log_callback, disp=False)
-
