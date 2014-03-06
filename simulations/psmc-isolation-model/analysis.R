0127f96b84f4610487b377335e31f1c420aa8aba
Thomas Mailund <mailund@birc.au.dk>
2014-03-06 13:27:12 +0100

    Constant size results.
    These estimates are so wildly wrong it is not even funny. I need to try with more data and then start with some debugging.

--- simulations/psmc-isolation-model/analysis.R
+++ simulations/psmc-isolation-model/analysis.R
@@ -1,17 +1,6 @@
 
-data <- rbind(read.table('estimates.split-0.001-mya.txt',header=TRUE),
-              read.table('estimates.split-0.1-mya.txt',header=TRUE),
-              read.table('estimates.split-1-mya.txt',header=TRUE),
-              read.table('estimates.split-2-mya.txt',header=TRUE),
-              read.table('estimates.split-4-mya.txt',header=TRUE))
+data <- read.table('constant-size-estimates.txt', header=FALSE)
+thetas <- data[,-ncol(data)]
 
-require(ggplot2)
 
-qplot(factor(sim.tau), mle.tau, data=data, geom='boxplot') +
-  geom_hline(yintercept=unique(data$sim.tau), col='red')
-
-qplot(factor(sim.tau), mle.theta, data=data, geom='boxplot') +
-  geom_hline(yintercept=unique(data$sim.theta), col='red')
-
-qplot(factor(sim.tau), mle.rho, data=data, geom='boxplot') +
-  geom_hline(yintercept=unique(data$sim.rho), col='red')
+plot(1:ncol(thetas), thetas[1,])
