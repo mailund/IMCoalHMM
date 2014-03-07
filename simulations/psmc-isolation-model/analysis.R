1caf224c44a047038f15b35b3e5dfaaca8622cd1
Thomas Mailund <mailund@birc.au.dk>
2014-03-07 08:49:12 +0100

    New visualisation.
    It isn't *that* bad after transforming the parameters to the right space ;)

--- simulations/psmc-isolation-model/analysis.R
+++ simulations/psmc-isolation-model/analysis.R
@@ -1,10 +1,15 @@
 library(reshape2)
 library(ggplot2)
+library(scales)
 
 data <- read.table('constant-size-estimates.txt', header=FALSE)
-thetas <- cbind(simulation=factor(1:nrow(data)), data[,-ncol(data)])
 
-qplot(variable, value, color=simulation, data=melt(thetas, id.vars='simulation'))
+# transforming since it wasn't done in the output when I simulated this data
+thetas <- cbind(simulation=factor(1:nrow(data)), 2/data[,-ncol(data)])
+names(thetas) <- c('simulation', 1:28)
 
-
+qplot(as.numeric(variable), value, color=simulation, #geom='line', 
+      data=melt(thetas, id.vars='simulation')) +
+  geom_hline(yintercept=0.001, col='red') + 
+  scale_y_continuous(trans=log10_trans(), limits=c(0.0001, 0.02))
 
