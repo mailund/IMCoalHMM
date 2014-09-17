7715df0c4ffabf6fc78c84f4ad2ca3a4e1e5e044
Svend Vendelbo Nielsen <svend1991@msn.com>
2014-09-17 15:57:38 +0200

    making more diverse switches

--- scripts/variable-migration-model-mcmc.py
+++ scripts/variable-migration-model-mcmc.py
@@ -139,9 +139,22 @@ recombination rate."""
     
     def switchRows(inarray):
         ans=[inarray[no_epochs*4]]*(no_epochs*4+1)
+        draw=randint(0,6)
+        if draw==0:
+            h=[1]
+        elif draw==1:
+            h=[2]
+        elif draw==2:
+            h=[1,2]
+        elif draw==3:
+            h=[1,3]
+        elif draw==4:
+            h=[2,4]
+        else:
+            h=[1,2,3,4]
         perm=permutation(no_epochs)
         for i in range(len(perm)):
-            for j in range(4):#the four categories c1,c2,mig12,mig21
+            for j in h:#a subset of the four categories c1,c2,mig12,mig21
                  ans[j*no_epochs+i]=inarray[j*no_epochs+perm[i]]
         return array(ans)
         
